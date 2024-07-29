e48833431e469b6330776c19977fcd92940c6b16
name <nothappysunchild@gmail.com>
2024-07-28 21:48:24 -0400

    remove tests

--- test_client.py
+++
@@ -1,9 +0,0 @@
-from websockets import WebSocketClientProtocol
-from socket2 import WebsocketClient
-
-class MyClient(WebsocketClient):
-	async def on_connect(self, socket: WebSocketClientProtocol):
-		print(socket)
-
-client = MyClient('ws://localhost:8080')
-client.connect()
