f58d0b33cc9a3cf22b5f3d4d45abc1f4e8cb35b5
name <nothappysunchild@gmail.com>
2024-07-29 16:39:43 -0400

    a

--- setup.py
+++ setup.py
@@ -1,17 +1,16 @@
 import setuptools
 
-setup_info = {
-	'name': 'socket2',
-	'version': '1.0.0',
-	'author': 'HappySunChild',
-	'description': 'websockets',
-	'url': 'https://github.com/HappySunChild',
-	'packages': setuptools.find_packages(),
-	'python_requires': '>=3.9',
-	'install_requires': [
+setuptools.setup(
+	name='socket2',
+	description='',
+	version='1.0.0',
+	author='HappySunChild',
+	url='https://github.com/HappySunChild/socket2',
+	
+	packages=setuptools.find_packages(),
+	python_requires='>=3.9',
+	install_requires=[
 		'websockets>=12.0',
-		'python-json>=2.0.9'
+		'json>=2.0.9'
 	]
-}
-
-setuptools.setup(**setup_info)
+)
